94fc3674757dc3a95e158690434a1d10159294a4
Simon Colmer <simon_colmer@hotmail.com>
2014-12-12 23:33:23 +0000

    Added nuget stuff

---
+++ FlameDeerBountyHunter.Domain/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace FlameDeerBountyHunter.Domain
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver _webDriver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void Login(string username, string password)
+        {
+            
+        }
+    }
+}
